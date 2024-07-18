bf0269f30582d1b0e53791051f740b46e85dbe4a
kathytran88 <kathyngananhtran@gmail.com>
2024-07-18 18:21:21 -0400

    Kathy Tran 1/ create a function that connects the SQL query with the backend to retrieve users’ information
    2/ Create 3 tables for Mentorship, User profiles, and Posts

--- user_model.sql
+++ user_model.sql
@@ -7,3 +7,27 @@ CREATE TABLE Users (
     role VARCHAR(10) NOT NULL CHECK (role IN ('Artist', 'Buyer')),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+/* Kathy Tran: Create 3 tables for Mentorship, User profiles, and Posts */
+
+CREATE TABLE Mentorship (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR(50) NOT NULL,
+    experience VARCHAR(500)
+);
+
+CREATE TABLE User profiles (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE Posts (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    art_image_tag VARCHAR(50),
+    artist_username VARCHAR(50), 
+    date_of_posting DATE, 
+    views INT, 
+    likes INT, 
+    interactions_amount_10sec_viewings INT, 
+    time_of_posting TIME,
+);
